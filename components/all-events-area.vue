7f427d22ca079db49812ffa95560bd36ec0a37b1
mariiachernonos <57815384+mariiachernonos@users.noreply.github.com>
2020-06-01 02:13:13 +0300

    Add files via upload

---
+++ components/all-events-area.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <event
+                v-for="(event, index) in events.length"
+                :key="event.title"
+                :index="index"
+                :events-array="events.map(e => e.title)"
+        />
+    </div>
+</template>
+
+<script>
+    import app from '../scripts/session-data.js'
+    import event from '../components/event.vue';
+    export default {
+        name: "all-events-area",
+        components:{
+            event
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
